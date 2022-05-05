771a90ddc5debb8dd07527c9daf9ff7be656c6f1
xtrm-en <26600206+xtrm-en@users.noreply.github.com>
2022-05-05 22:14:04 +0200

    patcher/test: add base test classes

---
+++ patcher/src/test/kotlin/fr/stardustenterprises/deface/patcher/tests/opcodePatching.kt
@@ -0,0 +1,11 @@
+package fr.stardustenterprises.deface.patcher.tests
+
+/**
+ * @see [PointPatchingTests]
+ *
+ * @author xtrm
+ * @since 0.4.0
+ */
+internal class OpcodePatchingTests {
+
+}
---
+++ patcher/src/test/kotlin/fr/stardustenterprises/deface/patcher/tests/pointPatching.kt
@@ -0,0 +1,15 @@
+package fr.stardustenterprises.deface.patcher.tests
+
+import fr.stardustenterprises.deface.api.patcher.IPatchPoint
+
+/**
+ * Tests focused on simple [IPatchPoint]s.
+ *
+ * @see [OpcodePatchingTests]
+ *
+ * @author xtrm
+ * @since 0.4.0
+ */
+internal class PointPatchingTests {
+
+}
